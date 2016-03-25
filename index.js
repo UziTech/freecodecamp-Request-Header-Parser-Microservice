035af212625c9fb9bdb286f96ba8a927334f1ad4
Tony Brix <Tony@Brix.ninja>
2016-03-24 20:05:20 -0500

    update

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 var express = require('express');
-var moment = require('moment');
+var requestIp = require('request-ip');
+var userAgentParser = require('user-agent-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(function (req, res) {
-	var input = decodeURIComponent(req.path.replace(/^\//, ""));
-	var time = {unix: null, natural: null};
-	var date;
-	if (!input.match(/\D/)) {
-		date = moment(input, "X");
-	} else {
-		date = moment(input, "MMMM D, YYYY");
-		if (date.format("MMMM D, YYYY") !== input) {
-			date = moment.invalid();
-		}
-	}
-	if (date.isValid()) {
-		time.unix = date.unix();
-		time.natural = date.format("MMMM D, YYYY");
-	}
-	res.json(time);
+	var parser = new userAgentParser();
+	var ua = req.headers['user-agent'];
+	var info = {
+		ipaddress: requestIp.getClientIp(req),
+		language: req.headers["accept-language"],
+		software: JSON.stringify(parser.setUA(ua).getResult())
+	};
+	res.json(info);
 });
 
 app.listen(app.get('port'), function () {
